6c084f77c8b255874eee26897dfb0da440767b15
Alejandro Ito Aramendia <alejandro.itoaramendia@gmail.com>
2024-12-08 12:17:52 +0900

    Add mongoose database connection for AWS lambda

---
+++ utils/db.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+let isConnected;
+
+export const connectToDatabase = async () => {
+  if (isConnected) {
+    console.log("=> using existing database connection");
+    return;
+  }
+
+  console.log("=> using new database connection");
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log("=> database connected");
+  } catch (error) {
+    console.error("=> database connection error", error);
+    throw error;
+  }
+};
